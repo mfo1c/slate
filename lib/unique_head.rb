6e0251e4e47b73b2ba2db8b9aebffd958a2dd5d2
mfo1c <info@mfo1c.ru>
2017-11-26 01:10:10 +0300

    Updates

--- lib/unique_head.rb
+++ lib/unique_head.rb
@@ -1,12 +1,13 @@
 # Unique header generation
 require 'middleman-core/renderers/redcarpet'
+require 'russian'
 class UniqueHeadCounter < Middleman::Renderers::MiddlemanRedcarpetHTML
   def initialize
     super
     @head_count = {}
   end
   def header(text, header_level)
-    friendly_text = text.parameterize
+    friendly_text = Russian::transliterate(text).parameterize
     @head_count[friendly_text] ||= 0
     @head_count[friendly_text] += 1
     if @head_count[friendly_text] > 1
